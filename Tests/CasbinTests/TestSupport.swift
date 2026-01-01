b1aea9324a201f6976ddab29c77874e42cee94e4
Yang Luo <hsluoyz@gmail.com>
2026-01-01 15:20:33 +0800

    feat: migrate to latest Swift 6.2 (#57)

--- Tests/CasbinTests/TestSupport.swift
+++ Tests/CasbinTests/TestSupport.swift
@@ -1,7 +1,7 @@
 import NIO
 
 // Shared helpers for test file paths and small utilities
-public let TestsfilePath = #file.components(separatedBy: "TestSupport.swift")[0]
+public let TestsfilePath = #filePath.components(separatedBy: "TestSupport.swift")[0]
 
 @inline(__always)
 func tryBool(_ body: () throws -> Bool) -> Bool { (try? body()) ?? false }
